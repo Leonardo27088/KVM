fd1f9c1f649b1101d606dc815b89f44e6a32db54
Leonardo <leonardofabian2708@gmail.com>
2026-04-09 21:50:49 +0200

    Initial commit

---
+++ MouseWin32.c
@@ -0,0 +1,31 @@
+#include <windows.h>
+#include <stdio.h>
+
+LRESULT CALLBACK LowLevelMouseProc(int nCode, WPARAM wParam, LPARAM lParam) {
+    MSLLHOOKSTRUCT* lp = (MSLLHOOKSTRUCT*)lParam;
+
+    if (wParam == WM_LBUTTONDOWN) {
+        printf("Click");
+    }
+
+    return CallNextHookEx(0, nCode, wParam, lParam);
+}
+
+int main() {
+    HHOOK mouseHandle = SetWindowsHookEx(WH_MOUSE_LL, LowLevelMouseProc, NULL, 0);
+
+    if (mouseHandle == NULL) {
+        printf("Error");
+        return 1;
+    }
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    UnhookWindowsHookEx(mouseHandle);
+
+    return 0;
+}
