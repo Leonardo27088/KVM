e91d01b38e06a2026ca0f6cf09dc1f0622a47604
leonardo <leonardofabian2708@gmail.com>
2026-05-08 20:58:53 +0200

    Implemented libevdev and duplicates virtual mouse succesfully

--- UDPClient.c
+++ UDPClient.c
@@ -31,6 +31,24 @@ static void check(int i) {
     }
 }
 
+struct input_absinfo abs_x = {
+    .value = 0,
+    .minimum = 0,
+    .maximum = 1600,
+    .fuzz = 0,
+    .flat = 0,
+    .resolution = 1
+};
+
+struct input_absinfo abs_y = {
+    .value = 0,
+    .minimum = 0,
+    .maximum = 900,
+    .fuzz = 0,
+    .flat = 0,
+    .resolution = 1
+};
+
 int main() {
     char buffer[DEFAULT_BUFLEN];
     char *message = "Hello server";
@@ -54,23 +72,24 @@ int main() {
     printf("Hello Server sent\n");
 
     struct libevdev* evdev = libevdev_new();
-                libevdev_set_name(evdev, "Virtual Mouse");
-                libevdev_set_id_vendor(evdev, 0x01);
-                libevdev_set_id_product(evdev, 0x01);
-                libevdev_set_id_version(evdev, 0x01);
-                libevdev_set_id_bustype(evdev, BUS_USB);
+    libevdev_set_name(evdev, "Virtual Mouse");
+    libevdev_set_id_vendor(evdev, 0x01);
+    libevdev_set_id_product(evdev, 0x01);
+    libevdev_set_id_version(evdev, 0x01);
+    libevdev_set_id_bustype(evdev, BUS_USB);
 
-                check(libevdev_enable_event_type(evdev, EV_REL));
-                check(libevdev_enable_event_code(evdev, EV_REL, REL_X, NULL));
-                check(libevdev_enable_event_code(evdev, EV_REL, REL_Y, NULL));
-                check(libevdev_enable_event_code(evdev, EV_REL, SYN_REPORT, NULL));
+    check(libevdev_enable_event_type(evdev, EV_ABS));
+    check(libevdev_enable_event_code(evdev, EV_ABS, ABS_X, &abs_x));
+    check(libevdev_enable_event_code(evdev, EV_ABS, ABS_Y, &abs_y));
+    check(libevdev_enable_event_code(evdev, EV_SYN, SYN_REPORT, 0));
 
-                check(libevdev_enable_event_type(evdev, EV_KEY));
-                check(libevdev_enable_event_code(evdev, EV_KEY, BTN_LEFT, NULL));
-                check(libevdev_enable_event_code(evdev, EV_KEY, BTN_RIGHT, NULL));
+    check(libevdev_enable_event_type(evdev, EV_KEY));
+    check(libevdev_enable_event_code(evdev, EV_KEY, BTN_LEFT, NULL));
+    check(libevdev_enable_event_code(evdev, EV_KEY, BTN_RIGHT, NULL));
+    libevdev_enable_event_code(evdev, EV_KEY, BTN_TOUCH, NULL);
 
-                struct libevdev_uinput* uinput;
-                check(libevdev_uinput_create_from_device(evdev, LIBEVDEV_UINPUT_OPEN_MANAGED, &uinput));
+    struct libevdev_uinput* uinput;
+    check(libevdev_uinput_create_from_device(evdev, LIBEVDEV_UINPUT_OPEN_MANAGED, &uinput));
 
     while (1) {
         int bytesRead = recvfrom(sockfd, &packet, sizeof(packet), 0, NULL, NULL);
@@ -80,17 +99,16 @@ int main() {
                 int targetX = (int)(packet.normX * 1600);
                 int targetY = (int)(packet.normY * 900);
 
-                check(libevdev_uinput_write_event(uinput, EV_REL, REL_X, targetX));
-                    check(libevdev_uinput_write_event(uinput, EV_REL, REL_Y, targetY));
-
-                    check(libevdev_uinput_write_event(uinput, EV_SYN, SYN_REPORT, 0));
+                check(libevdev_uinput_write_event(uinput, EV_ABS, ABS_X, targetX));
+                check(libevdev_uinput_write_event(uinput, EV_ABS, ABS_Y, targetY));
+                check(libevdev_uinput_write_event(uinput, EV_SYN, SYN_REPORT, 0));
             }
         }
-        // recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)NULL, NULL);
-        // puts(buffer);
     }
 
+    libevdev_uinput_destroy(uinput);
     close(sockfd);
+
 
     return 0;
 }
