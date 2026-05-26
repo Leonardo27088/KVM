2c76b13c603910b10ca42c5dd7c781b956909f39
leonardo <leonardofabian2708@gmail.com>
2026-05-26 18:50:55 +0200

    Changed typo AGAIN from libebdev to libevdev

--- Client/devices.h
+++ Client/devices.h
@@ -3,6 +3,8 @@
 #include <libevdev-1.0/libevdev/libevdev.h>
 #include <libevdev-1.0/libevdev/libevdev-uinput.h>
 
+void check(int i);
+
 struct libevdev_uinput* create_mouse();
 struct libevdev_uinput* create_touchpad();
-struct libebdev_uinput* create_keyboard();
+struct libevdev_uinput* create_keyboard();
