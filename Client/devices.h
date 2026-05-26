5fac9edc6d6a0928918e3c9f9c7873e3b7160ccb
leonardo <leonardofabian2708@gmail.com>
2026-05-26 18:20:37 +0200

    Added devices header file with create mouse, touchpad and keyboard functions

---
+++ Client/devices.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <libevdev-1.0/libevdev/libevdev.h>
+#include <libevdev-1.0/libevdev/libevdev-uinput.h>
+
+struct libevdev_uinput* create_mouse();
+struct livevdev_uinput* create_touchpad();
+struct livebdev_uinput* create_keyboard();
