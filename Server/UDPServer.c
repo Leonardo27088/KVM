365b47542133d2223e7e4cbfb1e9367813ec42c9
Leonardo <leonardofabian2708@gmail.com>
2026-05-26 17:25:34 +0200

    Removed create window function and added the necessary header file

--- Server/UDPServer.c
+++ Server/UDPServer.c
@@ -12,6 +12,7 @@
 #include <stdint.h>
 
 #include "protocol.h"
+#include "window.h"
 
 #pragma comment (lib, "Ws2_32.lib");
 
@@ -24,60 +25,10 @@ struct sockaddr_in SenderAddr;
 int SenderAddrSize = sizeof (SenderAddr);
 
 MSG msg;
-HWND hwnd;
-
-const wchar_t windowClassName[] = L"myWindowClass";
 
 bool isRemote = false;
 float virtualX = 1600.0f;
 float virtualY = 900.0f;
-
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-    switch (msg) {
-        case WM_CLOSE:
-            DestroyWindow(hwnd);
-            break;
-        case WM_DESTROY:
-            PostQuitMessage(0);
-            break;
-        default:
-            return DefWindowProc(hwnd, msg, wParam, lParam);
-    }
-
-    return 0;
-}
-
-BOOL CreateInvisibleWindow(HINSTANCE hInstance) {
-    WNDCLASSEX wcx = {0};
-
-    wcx.cbSize = sizeof(wcx);
-    wcx.style = CS_HREDRAW | CS_VREDRAW;
-    wcx.lpfnWndProc = WndProc;
-    wcx.hInstance = hInstance;
-    wcx.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wcx.lpszClassName = windowClassName;
-
-    if (!RegisterClassEx(&wcx)) {
-        return FALSE;
-    }
-
-    hwnd = CreateWindowEx(WS_EX_TOPMOST | WS_EX_LAYERED | WS_EX_TRANSPARENT,
-        windowClassName,
-        L"KVM",
-        WS_POPUP, 0, 0,
-        GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN),
-        NULL, NULL, hInstance, NULL
-    );
-
-    if (hwnd == NULL) {
-        return FALSE;
-    }
-
-    SetLayeredWindowAttributes(hwnd, 0, 0, LWA_ALPHA);
-
-    ShowWindow(hwnd, SW_HIDE);
-    return TRUE;
-}
 
 LRESULT CALLBACK LowLevelMouseProc(int nCode, WPARAM wParam, LPARAM lParam) {
     MSLLHOOKSTRUCT* lp = (MSLLHOOKSTRUCT*)lParam;
