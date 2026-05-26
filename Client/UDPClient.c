68e6cbf296ebd6f72bfd6737583f20aec1cf3adb
leonardo <leonardofabian2708@gmail.com>
2026-05-26 18:11:00 +0200

    Removed mouse and keyboard packet structs and included protocolClient.h

--- Client/UDPClient.c
+++ Client/UDPClient.c
@@ -11,29 +11,7 @@
 #include <libevdev-1.0/libevdev/libevdev-uinput.h>
 
 #include "keymap.h"
-
-#define PORT 27015
-#define DEFAULT_BUFLEN 512
-
-#pragma pack(push, 1)
-typedef struct {
-    uint8_t type;
-    float normX;
-    float normY;
-    uint16_t code;
-    int32_t value;
-} MousePacket;
-#pragma pack(pop)
-
-MousePacket packet;
-
-#pragma pack(push, 1)
-typedef struct {
-    uint8_t type;
-    uint32_t code;
-    int32_t value;
-} KeyPacket;
-#pragma pack(pop)
+#include "protocolClient.h"
 
 static void check(int i) {
     if (i < 0) {
